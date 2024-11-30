26a7b0ae9ffd1c9c6a4835345c7bcb97fe155881
Gonzalo Raymond <graymond.webdev@gmail.com>
2024-11-30 20:34:46 -0300

    Fix Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
+
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,9 +13,9 @@ export default defineConfig({
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public'}),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
     }
   },
 
-  adapter: vercel(),
+  adapter: vercel()
 });
